b6fc4d259c8c3acfefbbffd158c18937576dc299
mayankjustdial <mayankghz225@gmail.com>
2025-09-14 11:29:33 +0530

    add error handling and logging

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,17 +1,27 @@
 import { Repository } from 'typeorm'
 import { User } from '../entity/User'
 import { UserData } from '../types'
+import createHttpError from 'http-errors'
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
     async create({ firstName, lastName, email, password }: UserData) {
-        const user = await this.userRepository.save({
-            firstName,
-            lastName,
-            email,
-            password,
-        })
-        return user
+        try {
+            const user = await this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            })
+            return user
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                'Failed to store data in Database',
+            )
+            throw error
+        }
     }
 }
